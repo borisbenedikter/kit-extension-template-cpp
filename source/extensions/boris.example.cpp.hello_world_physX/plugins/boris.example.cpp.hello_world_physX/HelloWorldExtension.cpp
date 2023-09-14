98db7aa1b80ab32a6d5ea7aa4ea17e43153e170d
LorenzoFederici <federicilorenzo94@gmail.com>
2023-09-14 12:32:47 -0700

    hello world physX

--- source/extensions/boris.example.cpp.hello_world_physX/plugins/boris.example.cpp.hello_world_physX/HelloWorldExtension.cpp
+++ source/extensions/boris.example.cpp.hello_world_physX/plugins/boris.example.cpp.hello_world_physX/HelloWorldExtension.cpp
@@ -14,11 +14,165 @@
 #include <omni/ext/IExt.h>
 #include <omni/kit/IApp.h>
 
-const struct carb::PluginImplDesc pluginImplDesc = { "boris.example.cpp.hello_world_physX.plugin",
-                                                     "An example C++ extension.", "NVIDIA",
-                                                     carb::PluginHotReload::eEnabled, "dev" };
+#include "PxConfig.h"
+#include "PxPhysicsAPI.h"
+#include "snippetcommon/SnippetPrint.h"
+#include "snippetcommon/SnippetPVD.h"
+#include "snippetutils/SnippetUtils.h"
+
+using namespace physx;
+
+const struct carb::PluginImplDesc pluginImplDesc = {
+	"boris.example.cpp.hello_world_physX.plugin",
+	"An example C++ extension.", "NVIDIA",
+	carb::PluginHotReload::eEnabled, "dev"};
 
 CARB_PLUGIN_IMPL_DEPS(omni::kit::IApp)
+
+// -------------------------------------------------------------------------- //
+// ------------------------------- Callbacks -------------------------------- //
+// -------------------------------------------------------------------------- //
+
+// Usually, the allocator callback and error callback are specific to the application,
+// but PhysX provides default implementations that make it easy to get started.
+
+// PhysX performs all allocations via the PxAllocatorCallback interface.
+// You must implement this interface in order to initialize PhysX:
+static PxDefaultAllocator		gAllocator;
+
+// PhysX logs all error messages through the PxErrorCallback interface.
+// You must implement this interface in order to initialize PhysX:
+static PxDefaultErrorCallback	gErrorCallback;
+
+static PxFoundation*			gFoundation = NULL;
+static PxPhysics*				gPhysics	= NULL;
+static PxDefaultCpuDispatcher*	gDispatcher = NULL;
+static PxScene*					gScene		= NULL;
+static PxMaterial*				gMaterial	= NULL;
+static PxPvd*					gPvd        = NULL;
+
+static PxReal stackZ = 10.0f;
+
+static PxRigidDynamic *createDynamic(const PxTransform &t,
+									 const PxGeometry &geometry,
+									 const PxVec3 &velocity = PxVec3(0))
+{
+	PxRigidDynamic* dynamic = PxCreateDynamic(*gPhysics, t, geometry, *gMaterial, 10.0f);
+	dynamic->setAngularDamping(0.5f);
+	dynamic->setLinearVelocity(velocity);
+	gScene->addActor(*dynamic);
+	return dynamic;
+}
+
+static void createStack(const PxTransform& t, PxU32 size, PxReal halfExtent)
+{
+	PxShape* shape = gPhysics->createShape(PxBoxGeometry(halfExtent, halfExtent, halfExtent), *gMaterial);
+	for(PxU32 i=0; i<size;i++)
+	{
+		for(PxU32 j=0;j<size-i;j++)
+		{
+			PxTransform localTm(PxVec3(PxReal(j*2) - PxReal(size-i), PxReal(i*2+1), 0) * halfExtent);
+			PxRigidDynamic* body = gPhysics->createRigidDynamic(t.transform(localTm));
+			body->attachShape(*shape);
+			PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+			gScene->addActor(*body);
+		}
+	}
+	shape->release();
+}
+
+void initPhysics(bool interactive)
+{
+	// Every PhysX module requires a PxFoundation instance to be available
+	// Inputs are:
+	// - SDK version we are using
+	// - Allocator callback
+	// - Error callback
+	gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+
+	// The recordMemoryAllocations parameter specifies whether to perform memory profiling.
+	bool recordMemoryAllocations = true;
+
+	// Create the PhysX Visual Debugger (PVD)
+	gPvd = PxCreatePvd(*gFoundation);
+	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+	gPvd->connect(*transport,PxPvdInstrumentationFlag::eALL);
+
+	// Create the (top-level?) PxPhysics object
+	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), recordMemoryAllocations, gPvd);
+
+	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+	gDispatcher = PxDefaultCpuDispatcherCreate(2);
+	sceneDesc.cpuDispatcher	= gDispatcher;
+	sceneDesc.filterShader	= PxDefaultSimulationFilterShader;
+	gScene = gPhysics->createScene(sceneDesc);
+
+	PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+	if(pvdClient)
+	{
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+	}
+	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+	PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0,1,0,0), *gMaterial);
+	gScene->addActor(*groundPlane);
+
+	for(PxU32 i=0;i<5;i++)
+		createStack(PxTransform(PxVec3(0,0,stackZ-=10.0f)), 10, 2.0f);
+
+	if(!interactive)
+		createDynamic(PxTransform(PxVec3(0,40,100)), PxSphereGeometry(10), PxVec3(0,-50,-100));
+}
+
+void stepPhysics(bool /*interactive*/)
+{
+	gScene->simulate(1.0f/60.0f);
+	gScene->fetchResults(true);
+}
+
+void cleanupPhysics(bool /*interactive*/)
+{
+	PX_RELEASE(gScene);
+	PX_RELEASE(gDispatcher);
+	PX_RELEASE(gPhysics);
+	if(gPvd)
+	{
+		PxPvdTransport* transport = gPvd->getTransport();
+		gPvd->release();	gPvd = NULL;
+		PX_RELEASE(transport);
+	}
+	PX_RELEASE(gFoundation);
+
+	printf("SnippetHelloWorld done.\n");
+}
+
+void keyPress(unsigned char key, const PxTransform& camera)
+{
+	switch(toupper(key))
+	{
+	case 'B':	createStack(PxTransform(PxVec3(0,0,stackZ-=10.0f)), 10, 2.0f);						break;
+	case ' ':	createDynamic(camera, PxSphereGeometry(3.0f), camera.rotate(PxVec3(0,0,-1))*200);	break;
+	}
+}
+
+int snippetMain(int, const char*const*)
+{
+#ifdef RENDER_SNIPPET
+	extern void renderLoop();
+	renderLoop();
+#else
+	static const PxU32 frameCount = 100;
+	initPhysics(false);
+	for(PxU32 i=0; i<frameCount; i++)
+		stepPhysics(false);
+	cleanupPhysics(false);
+#endif
+
+	return 0;
+}
 
 namespace boris
 {
@@ -48,6 +202,9 @@ public:
                 onUpdate();
             });
         }
+
+        // PhysX snippet code main
+        snippetMain(0, NULL);
     }
 
     void onShutdown() override
--- source/extensions/boris.example.cpp.hello_world_physX/premake5.lua
+++ source/extensions/boris.example.cpp.hello_world_physX/premake5.lua
@@ -15,4 +15,58 @@ repo_build.prebuild_link {
 project_ext_plugin(ext, "boris.example.cpp.hello_world_physX.plugin")
     local plugin_name = "boris.example.cpp.hello_world_physX"
     add_files("source", "plugins/"..plugin_name)
-    includedirs { "plugins/"..plugin_name }
+    includedirs { "plugins/"..plugin_name , "../../../extern/PhysX/physx/include" , "../../../extern/PhysX/physx/snippets" }
+    libdirs {
+        "../../../extern/PhysX/physx/bin/linux.clang/checked",
+        "/usr/local/cuda/lib64",  -- here there is only cudart, but no cuda
+        os.findlib("cuda")
+    }
+    links {
+        "PhysX_static_64",
+        "PhysXPvdSDK_static_64",
+        "PhysXExtensions_static_64",
+        "PhysXCooking_static_64",
+        "PhysXCommon_static_64",
+        "PhysXFoundation_static_64",
+        "PhysXVehicle_static_64",
+        "PhysXVehicle2_static_64",
+        "PhysXCharacterKinematic_static_64",
+        "SnippetUtils_static_64",
+        "PVDRuntime_64",
+        "PhysXGpu_64",
+    }
+        -- "cuda"
+
+        -- "cuda"
+    -- }
+    -- defines { "NOMINMAX", "NDEBUG" }
+    -- runtime "Release"
+    -- rtti "On"
+
+
+    -- filter { "system:linux" }
+    --     exceptionhandling "On"
+    --     staticruntime "Off"
+    --     cppdialect "C++17"
+    --     includedirs { "%{target_deps}/python/include/python3.10" }
+    --     buildoptions { "-D_GLIBCXX_USE_CXX11_ABI=0 -Wno-deprecated-declarations -Wno-deprecated -Wno-unused-variable -pthread -lstdc++fs -Wno-undef" }
+    --     linkoptions { "-Wl,--disable-new-dtags -Wl,-rpath,%{target_deps}/nv_usd/release/lib:%{target_deps}/python/lib:%{target_deps}/usd_ext_physics/release/lib:" }
+    -- filter { "system:windows" }
+    --     buildoptions { "/wd4244 /wd4305" }
+    -- filter {}
+
+    -- links {
+    --     "PhysX",
+    --     "PhysXPvdSDK",
+    --     "PhysXExtensions",
+    --     "PhysXCooking",
+    --     "PhysXCommon",
+    --     "PhysXFoundation",
+    --     "PhysXVehicle",
+    --     "PhysXVehicle2",
+    --     "PhysXCharacterKinematic",
+    --     "SnippetUtils",
+    --     "PVDRuntime",
+    --     "PhysXGpu",
+    --     "cuda"
+    -- }
